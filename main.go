88804580cc1c7e1b05a2de9f936f45928cd918b5
anithhhh <anisztalib@gmail.com>
2024-11-07 00:46:40 +0800

    Initial commit of the booking-app project

--- main.go
+++ main.go
@@ -1,38 +1,132 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
+
+const conferenceTickets int = 50
+var remainingTickets uint = 50
+var conferenceName = "Go Conference"
+var bookings = make([]userData, 0)
+
+type userData struct {
+	firstName      string
+	lastName       string
+	email          string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
+var mu = sync.Mutex{} // Mutex to protect remainingTickets and bookings
 
 func main() {
-	conferenceName := "Go Conference"
-	const conferenceTickets int = 50 
-	var remainingTickets unt = 50 
+	greetUsers()
 
+	for {
+		firstName, lastName, email, userTickets := getUserInput()
+		fmt.Printf("DEBUG: Received user input - FirstName: %v, LastName: %v, Email: %v, Tickets: %v\n", firstName, lastName, email, userTickets)
+
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		fmt.Printf("DEBUG: Validation results - isValidName: %v, isValidEmail: %v, isValidTicketNumber: %v\n", isValidName, isValidEmail, isValidTicketNumber)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			bookTicket(userTickets, firstName, lastName, email)
+
+			wg.Add(1)
+			go sendTicket(userTickets, firstName, lastName, email)
+
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("The first name or last name you entered is too short.")
+			}
+			if !isValidEmail {
+				fmt.Println("The email address you entered doesn't contain the '@' sign.")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("The number of tickets you entered is invalid.")
+			}
+		}
+	}
+	wg.Wait()
+}
+
+func greetUsers() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+}
 
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	return firstNames
+}
 
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
-	//ask user for their name
+
 	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	fmt.Scanln(&firstName)
 
 	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+	fmt.Scanln(&lastName)
 
 	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+	fmt.Scanln(&email)
 
 	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+	fmt.Scanln(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets 
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	return firstName, lastName, email, userTickets
 }
 
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-  
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	mu.Lock() // Lock shared resource
+	remainingTickets = remainingTickets - userTickets
+
+	var userData = userData{
+		firstName:      firstName,
+		lastName:       lastName,
+		email:          email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("DEBUG: Current list of bookings: %v\n", bookings)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	mu.Unlock() // Unlock shared resource
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(5 * time.Second) // Reduced wait time for testing purposes
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("###################")
+	fmt.Printf("Sending ticket: \n %v \nto email address %v\n", ticket, email)
+	fmt.Println("###################")
+	wg.Done()
+}
